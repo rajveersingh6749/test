655fd527069260f217d39af574c48cf2d54e9d7c
Rajveer Singh <777rajveersingh@gmail.com>
2025-09-02 11:03:36 +0530

    fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import cors from 'cors';
+// import cors from 'cors';
 
 dotenv.config({
      path: './.env' 
@@ -8,7 +8,7 @@ dotenv.config({
 )
 
 const app = express();
-app.use(cors())
+// app.use(cors())
 const PORT = process.env.PORT || 3000;
 
 const jokes = [
